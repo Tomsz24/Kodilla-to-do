6aba256999e09922d0e5ebd0daba2c9320bc9b71
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2021-03-11 18:39:17 +0100

    wrapped components List and Search by Container component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import Container from '../Container/Container.js';
 import Column from '../Column/ColumnContainer.js';
 import Creator from '../Creator/Creator.js';
 import ReactHtmlParser from 'react-html-parser';
@@ -24,22 +25,24 @@ class List extends React.Component {
     const { title, image, description, columns, addColumn } = this.props;
 
     return (
-      <section className={styles.component}>
-        <Hero titleText={title} imageSrc={image}></Hero>
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
+      <Container>
+        <section className={styles.component}>
+          <Hero titleText={title} imageSrc={image}></Hero>
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
 
-        <div className={styles.columns}>
-          {columns.map((columnData) => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
+          <div className={styles.columns}>
+            {columns.map((columnData) => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
 
-        <div className={styles.creator}>
-          <Creator action={addColumn} />
-        </div>
-      </section>
+          <div className={styles.creator}>
+            <Creator action={addColumn} />
+          </div>
+        </section>
+      </Container>
     );
   }
 }
